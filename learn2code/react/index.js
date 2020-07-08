f0156beca6ffde1250673d4c7db65633148ad080
Andrej Šebeň <andrej.seben@millennium.sk>
2020-07-08 11:05:32 +0200

    map shows to <li> tag

--- learn2code/react/index.js
+++ learn2code/react/index.js
@@ -3,7 +3,12 @@ class App extends React.Component {
     super(props)
     
     this.state =  {
-      show: 'Somali'
+      show: 'Somali',
+      shows: [
+        {id: 1, title: 'Somali', descr: 'Lost human', nOfEps: 12},
+        {id: 2, title: 'Rick and Morty', descr: 'You know this show', nOfEps: 20},
+        {id: 3, title: 'Mr. Robot', descr: 'Sample description. Hacker', nOfEps: 11}
+      ]
     }
   }
 
@@ -15,9 +20,13 @@ class App extends React.Component {
 
   render() {
     const { show } = this.state
+    const shows = this.state.shows.map(show => (<li key={show.id}>{show.title}</li>))
     return <div>
+      <ul>
+        {shows}
+      </ul>
       <form className="add-new">
-        <input type="text" onChange={this.onChange} />
+        <input type="text" onChange={this.onChange} value={show} />
       </form>
 
       <p className="preview">{show}</p>
