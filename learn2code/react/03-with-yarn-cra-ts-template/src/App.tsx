8a9aeb84890972f55ce1e77ca73bbe9b5a696683
OneManDevz <42937562+OneManDevz@users.noreply.github.com>
2020-07-11 16:37:02 +0200

    react router, switch, navlinks

--- learn2code/react/03-with-yarn-cra-ts-template/src/App.tsx
+++ learn2code/react/03-with-yarn-cra-ts-template/src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import logo from './assets/logo.svg'
 import './App.scss'
+import { Switch, Route, NavLink } from 'react-router-dom'
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+			<header>
+				<nav>
+					<NavLink to="/" exact>Home</NavLink>
+					<NavLink to="/itunes">iTunes</NavLink>
+					<NavLink to="/about">About</NavLink>
+				</nav>
 			</header>
+			<main className="content">
+				<Switch>
+					<Route path="/" render={() => <h1>Homepage</h1>} exact />
+					<Route path="/itunes" render={() => <h1>iTunes</h1>} />
+					<Route path="/about" render={() => <h1>About</h1>} />
+				</Switch>
+			</main>
 		</div>
 	)
 }
