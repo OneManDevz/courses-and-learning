85d9dcf544abb29f2082dd89cb6d186832609a6e
Andrej Šebeň <andrej.seben@millennium.sk>
2020-08-20 12:32:40 +0200

    app composition

--- learn2code/react_vue_angular/vuejs/00-with-yarn-vue-create-3-ts/src/views/About.vue
+++ learn2code/react_vue_angular/vuejs/00-with-yarn-vue-create-3-ts/src/views/About.vue
@@ -1,5 +1,77 @@
 <template>
-	<div class="about">
+	<article class="about">
 		<h1>This is an about page</h1>
-	</div>
+
+		<p>
+			Donec non erat fermentum, bibendum erat vel, suscipit est. Phasellus sodales,
+			urna at aliquet fermentum, velit felis aliquet justo, id rhoncus diam mi a
+			dui. In lorem est, suscipit sit amet odio at, varius sollicitudin quam. Proin
+			consequat eu lorem ac cursus. Quisque interdum scelerisque libero, sed
+			elementum ipsum. In quis risus eu justo dignissim cursus sed eu odio. Cras
+			vehicula, felis rhoncus tempus consequat, magna turpis vulputate ex, quis
+			rutrum quam tellus in mauris. Nam finibus turpis sit amet diam tempor, eget
+			sodales lectus feugiat. Aliquam sit amet diam ut nisi rutrum tempus. Nulla ut
+			est id urna cursus elementum ac id lorem. Nullam id libero placerat, semper
+			massa in, cursus mi.
+		</p>
+
+		<p>
+			Vestibulum tempor erat eu convallis mattis. Donec varius arcu vel tortor
+			volutpat, non egestas enim semper. Aenean sollicitudin urna semper lobortis
+			molestie. Fusce in nibh augue. Sed cursus ut eros non pretium. Quisque eget
+			risus ut libero tempus congue nec id dui. Ut est mi, ultrices in leo vel,
+			cursus luctus tellus. Pellentesque habitant morbi tristique senectus et netus
+			et malesuada fames ac turpis egestas. Praesent eget facilisis diam. Sed
+			bibendum, dolor in fringilla venenatis, ante magna mattis massa, a accumsan
+			risus tortor eget augue. Suspendisse mollis diam aliquam dolor tristique
+			faucibus at vel nisl. Mauris quam risus, posuere ac metus in, tincidunt
+			vestibulum mauris. Praesent mattis justo tellus, pellentesque maximus dui
+			rutrum in. Proin sapien libero, fermentum ut eleifend suscipit, semper eget
+			leo. Vivamus in turpis sodales, iaculis ipsum tristique, sodales tortor.
+			Phasellus tristique ultrices nisi, ac cursus eros vestibulum sit amet.
+		</p>
+
+		<p>
+			Sed tempor, enim in aliquam blandit, metus ex condimentum nulla, et euismod
+			ipsum sem at dolor. Suspendisse porta sapien et varius aliquet. Nunc purus
+			sapien, tincidunt ut blandit ut, elementum ac diam. Duis magna nunc, venenatis
+			id sapien nec, varius ornare neque. Sed nulla diam, venenatis consequat velit
+			id, dignissim viverra lacus. Integer suscipit metus lorem. Morbi quis mauris
+			ligula. Curabitur tincidunt magna sit amet lobortis elementum. Etiam in
+			volutpat tellus. Aliquam erat volutpat. Curabitur cursus mattis risus, ut
+			sollicitudin quam euismod sed. Suspendisse potenti. Donec nec pellentesque
+			quam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+			cubilia curae; In lacus enim, pellentesque nec egestas non, imperdiet a
+			tortor. Vestibulum sapien ligula, laoreet eget nisi vitae, euismod venenatis
+			diam.
+		</p>
+
+		<p>
+			Donec iaculis id elit eu pretium. Integer purus eros, volutpat id ornare eget,
+			finibus et tellus. Etiam quis tortor suscipit elit tristique facilisis. Donec
+			posuere aliquet eros sit amet convallis. Duis a facilisis est, nec ornare
+			ligula. Suspendisse consequat lectus at pulvinar bibendum. Suspendisse ac
+			augue ac nisl mattis iaculis. Cras in interdum elit, a aliquam tellus.
+		</p>
+
+		<p>
+			Aenean mauris diam, facilisis at odio a, tristique feugiat sem. Quisque id
+			justo ultrices, congue tellus non, dictum elit. Nulla egestas massa a risus
+			rhoncus, in interdum turpis pharetra. Proin sed auctor enim. In nec quam ex.
+			Fusce malesuada justo purus, et posuere purus placerat condimentum. Vestibulum
+			ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+			Aliquam in finibus est. Integer tempus libero id fermentum volutpat. Sed sit
+			amet feugiat arcu. Donec sit amet aliquam nisi, sed posuere leo.
+		</p>
+
+		<p>
+			Pellentesque non fermentum dolor, nec consequat arcu. Morbi viverra id ex id
+			mattis. Integer finibus ex quis varius accumsan. Nam varius elit odio, a
+			egestas eros bibendum et. Cras condimentum malesuada ligula commodo maximus.
+			Nulla facilisi. Quisque mattis vulputate lectus sed blandit. Cras euismod
+			facilisis lacus id vulputate. Nulla eget facilisis justo, sed sodales massa.
+			Curabitur imperdiet magna vitae nulla elementum, sit amet suscipit elit
+			posuere.
+		</p>
+	</article>
 </template>
---
+++ learn2code/react_vue_angular/vuejs/00-with-yarn-vue-create-3-ts/src/views/Tunes.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<h1>Tunes</h1>
+		<p>Brekeke</p>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
