c20bc03a499baceb873077063683bcd2657d5b59
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-12-01 16:10:46 +0100

    2019 part 1

---
+++ advent-of-code-2019/day01/part1.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let data = fs.readFileSync('puzzle-input.txt', 'utf-8');
+let stringNumbers = data.split(/\r?\n/);
+let numbers = [];
+
+stringNumbers.forEach((string) => {
+  numbers.push(parseInt(string));
+});
+
+let totalFuel = 0;
+
+numbers.forEach((mass) => {
+  totalFuel += Math.floor(mass / 3) - 2;
+});
+
+console.log(totalFuel);
