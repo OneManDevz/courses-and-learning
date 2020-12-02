89bd04324e684a08c9c75764afba121d34b92100
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-12-02 10:46:01 +0100

    2020 day 02 part 2

---
+++ advent-of-code-2020/day02/part2.js
@@ -0,0 +1,23 @@
+let fs = require('fs');
+let data = fs.readFileSync('puzzle-input.txt', 'utf-8');
+let lines = data.split(/\r?\n/);
+let correctCount = 0;
+
+lines.forEach((line) => {
+  let lineSplit = line.split(' ');
+  let minMax = lineSplit[0].split('-');
+  lineSplit[2][minMax[0] + 1];
+
+  if (
+    (lineSplit[2][minMax[0] - 1] == lineSplit[1][0] ||
+      lineSplit[2][minMax[1] - 1] == lineSplit[1][0]) &&
+    !(
+      lineSplit[2][minMax[0] - 1] == lineSplit[1][0] &&
+      lineSplit[2][minMax[1] - 1] == lineSplit[1][0]
+    )
+  ) {
+    correctCount++;
+  }
+});
+
+console.log(correctCount);
