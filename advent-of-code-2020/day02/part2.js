10aced3b61380ba8d5beee94538366b991baca87
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-12-02 14:28:28 +0100

    part2 XOR operator

--- advent-of-code-2020/day02/part2.js
+++ advent-of-code-2020/day02/part2.js
@@ -6,18 +6,12 @@ let correctCount = 0;
 lines.forEach((line) => {
   let lineSplit = line.split(' ');
   let minMax = lineSplit[0].split('-');
-  lineSplit[2][minMax[0] + 1];
 
   if (
-    (lineSplit[2][minMax[0] - 1] == lineSplit[1][0] ||
-      lineSplit[2][minMax[1] - 1] == lineSplit[1][0]) &&
-    !(
-      lineSplit[2][minMax[0] - 1] == lineSplit[1][0] &&
-      lineSplit[2][minMax[1] - 1] == lineSplit[1][0]
-    )
-  ) {
+    (lineSplit[2][minMax[0] - 1] == lineSplit[1][0]) ^
+    (lineSplit[2][minMax[1] - 1] == lineSplit[1][0])
+  )
     correctCount++;
-  }
 });
 
 console.log(correctCount);
