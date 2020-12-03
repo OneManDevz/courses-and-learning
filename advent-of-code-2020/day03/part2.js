e973298e4123bf86576d827ea57cabf1daf09ec7
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-12-03 10:22:18 +0100

    part2 simplified

--- advent-of-code-2020/day03/part2.js
+++ advent-of-code-2020/day03/part2.js
@@ -3,44 +3,22 @@ let data = fs.readFileSync('puzzle-input.txt', 'utf-8');
 let lines = data.split(/\r?\n/);
 
 const expandedLines = lines.map((line) => {
-  return line.repeat(Math.round((lines.length / lines[0].length) * 15));
+  return line.repeat(Math.round((lines.length / lines[0].length) * 7));
 });
 
 let count = 0;
 let count2 = 0;
 let count3 = 0;
 let count4 = 0;
-let x = 0;
-let y = 0;
 
-for (let i = 0; i < expandedLines.length - 1; i++) {
-  x += 3;
-  y += 1;
-  if (expandedLines[y][x] == '#') count++;
+for (let i = 1; i < expandedLines.length; i++) {
+  if (expandedLines[i][i * 3] == '#') count++;
+  if (expandedLines[i][i * 5] == '#') count2++;
+  if (expandedLines[i][i * 7] == '#') count3++;
 }
 
-x = 0;
-y = 0;
-for (let i = 0; i < expandedLines.length - 1; i++) {
-  x += 5;
-  y += 1;
-  if (expandedLines[y][x] == '#') count2++;
-}
-
-x = 0;
-y = 0;
-for (let i = 0; i < expandedLines.length - 1; i++) {
-  x += 7;
-  y += 1;
-  if (expandedLines[y][x] == '#') count3++;
-}
-
-x = 0;
-y = 0;
-for (let i = 0; i < expandedLines.length / 2 - 1; i++) {
-  x += 1;
-  y += 2;
-  if (expandedLines[y][x] == '#') count4++;
+for (let i = 1; i < expandedLines.length / 2; i++) {
+  if (expandedLines[i * 2][i] == '#') count4++;
 }
 
 console.log('Total:', count * count2 * count3 * count4);
