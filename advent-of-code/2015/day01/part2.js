90aa542e1aabc445b98d46a9c22e48a609db1862
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-12-04 03:14:54 +0100

    2015 day 01

---
+++ advent-of-code/2015/day01/part2.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const data = fs.readFileSync('input.txt', 'utf-8');
+
+let floor = 0;
+let basement = 0;
+
+for (let i = 0; i < data.length; i++) {
+  if (data[i] === ')') floor--;
+  if (data[i] === '(') floor++;
+
+  if (floor == -1) {
+    basement = i + 1;
+    break;
+  }
+}
+
+console.log(floor, basement);
