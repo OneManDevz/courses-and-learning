4080617acb2cc08fe05b93295e4d977abde22a76
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-12-04 08:12:04 +0100

    beautify - not for felix

--- advent-of-code/2020/day04/part1.js
+++ advent-of-code/2020/day04/part1.js
@@ -1,19 +1,15 @@
-const lines = require('fs')
-	.readFileSync('input.txt', 'utf-8')
-	.split(/\n\s*\n/)
-
-console.log('🚀 ~ file: part1.js ~ line 2 ~ lines', lines.length)
-
-const passports = lines.filter((passport) => {
-	return (
-		passport.includes('byr:') &&
-		passport.includes('iyr:') &&
-		passport.includes('eyr:') &&
-		passport.includes('hgt:') &&
-		passport.includes('hcl:') &&
-		passport.includes('ecl:') &&
-		passport.includes('pid:')
-	)
-})
-
-console.log('passports.length', passports.length)
+console.log(
+	require('fs')
+		.readFileSync('input.txt', 'utf-8')
+		.split(/\n\s*\n/)
+		.filter(
+			(passport) =>
+				passport.includes('byr:') &&
+				passport.includes('iyr:') &&
+				passport.includes('eyr:') &&
+				passport.includes('hgt:') &&
+				passport.includes('hcl:') &&
+				passport.includes('ecl:') &&
+				passport.includes('pid:')
+		).length
+)
