8cae0065cb6e25c0332e8ce16994517a621ccc7d
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-12-04 05:48:49 +0100

    2019 day02 part 2

--- advent-of-code/2019/day02/part2.js
+++ advent-of-code/2019/day02/part2.js
@@ -1,20 +1,30 @@
-const data = require('fs').readFileSync('input.txt', 'utf-8').split(',').map(Number)
+const initialData = require('fs').readFileSync('input.txt', 'utf-8').split(',').map(Number)
 
-data[1] = 12
-data[2] = 2
-let pointer = 0
+initialData[1] = 12
+initialData[2] = 2
 
-while (data[pointer] !== 99) {
-	if (data[pointer] == 1) {
-		data[data[pointer + 3]] = data[data[pointer + 1]] + data[data[pointer + 2]]
-	} else if (data[pointer] == 2) {
-		data[data[pointer + 3]] = data[data[pointer + 1]] * data[data[pointer + 2]]
+for (let noun = 0; noun < 100; noun++) {
+	for (let verb = 0; verb < 100; verb++) {
+		const data = [...initialData]
+
+		let pointer = 0
+
+		data[1] = noun
+		data[2] = verb
+
+		while (data[pointer] !== 99) {
+			if (data[pointer] == 1) {
+				data[data[pointer + 3]] = data[data[pointer + 1]] + data[data[pointer + 2]]
+			} else if (data[pointer] == 2) {
+				data[data[pointer + 3]] = data[data[pointer + 1]] * data[data[pointer + 2]]
+			}
+
+			pointer += 4
+		}
+
+		if (data[0] === 19690720) {
+			console.log(100 * noun + verb)
+			return
+		}
 	}
-
-	if (data[data[pointer + 3]] == 19690720) {
-		console.log('Daco')
-	}
-	pointer += 4
 }
-
-console.log('🚀 ~ file: part1.js ~ line 5 ~ data', data[0])
