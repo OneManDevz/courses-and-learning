807326268060fff96b045d591ce930e1054c4aa4
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-12-04 04:30:16 +0100

    2020 day01 mini fixes

--- advent-of-code/2020/day01/part1.js
+++ advent-of-code/2020/day01/part1.js
@@ -1,17 +1,13 @@
-let fs = require('fs')
-let data = fs.readFileSync('puzzle-input.txt', 'utf-8')
-let stringNumbers = data.split(/\r?\n/)
-let numbers = []
+let numbers = require('fs')
+	.readFileSync('puzzle-input.txt', 'utf-8')
+	.split(/\r?\n/)
+	.map((string) => parseInt(string))
 
-stringNumbers.forEach((string) => {
-	numbers.push(parseInt(string))
-})
-
-for (let i = 0; i < numbers.length; i++) {
+loop1: for (let i = 0; i < numbers.length; i++) {
 	for (let j = 1; j < numbers.length - 1; j++) {
 		if (numbers[i] + numbers[j] == 2020) {
 			console.log(numbers[i] * numbers[j])
-			return
+			break loop1
 		}
 	}
 }
--- advent-of-code/2020/day01/part2.js
+++ advent-of-code/2020/day01/part2.js
@@ -1,18 +1,14 @@
-let fs = require('fs')
-let data = fs.readFileSync('puzzle-input.txt', 'utf-8')
-let stringNumbers = data.split(/\r?\n/)
-let numbers = []
+let numbers = require('fs')
+	.readFileSync('puzzle-input.txt', 'utf-8')
+	.split(/\r?\n/)
+	.map((string) => parseInt(string))
 
-stringNumbers.forEach((string) => {
-	numbers.push(parseInt(string))
-})
-
-for (let i = 0; i < numbers.length; i++) {
+loop1: for (let i = 0; i < numbers.length; i++) {
 	for (let j = 1; j < numbers.length - 1; j++) {
 		for (let k = 2; k < numbers.length - 2; k++) {
 			if (numbers[i] + numbers[j] + numbers[k] == 2020) {
 				console.log(numbers[i] * numbers[j] * numbers[k])
-				return
+				break loop1
 			}
 		}
 	}
