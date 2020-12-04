dbaa2da1784a39a47474a1fda2ce53a7ef799f06
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-12-04 05:20:06 +0100

    2019 day02 part1

--- advent-of-code/2019/day02/part1.js
+++ advent-of-code/2019/day02/part1.js
@@ -1,1 +1,18 @@
-const fs = require('fs')
+const data = require('fs')
+	.readFileSync('input.txt', 'utf-8')
+	.split(',')
+	.map((a) => parseInt(a))
+
+data[1] = 12
+data[2] = 2
+
+for (let i = 0; i < data.length; i += 4) {
+	if (data[i] == 1) {
+		data[data[i + 3]] = data[data[i + 1]] + data[data[i + 2]]
+	} else if (data[i] == 2) {
+		data[data[i + 3]] = data[data[i + 1]] * data[data[i + 2]]
+	} else if (data[i] == 99) {
+		break
+	}
+}
+console.log('🚀 ~ file: part1.js ~ line 5 ~ data', data[0])
