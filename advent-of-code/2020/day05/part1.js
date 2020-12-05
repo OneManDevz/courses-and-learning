a9299aeb51b3d2b977ea57c2c70baa247843abc4
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-12-05 11:04:27 +0100

    day05 ref

--- advent-of-code/2020/day05/part1.js
+++ advent-of-code/2020/day05/part1.js
@@ -1,40 +1,41 @@
-const lines = require('fs').readFileSync('input.txt', 'utf-8').split(/\r?\n/)
 let high = 0
+require('fs')
+	.readFileSync('input.txt', 'utf-8')
+	.split(/\r?\n/)
+	.forEach((line) => {
+		let rows = []
+		let columns = []
 
-lines.forEach((line) => {
-	let rows = []
-	let columns = []
-
-	for (let r = 1; r <= 128; r++) {
-		rows.push(r)
-	}
-	for (let c = 1; c <= 8; c++) {
-		columns.push(c)
-	}
-
-	for (let i = 0; i < line.length; i++) {
-		let rowHalf = Math.ceil(rows.length / 2)
-		let colHalf = Math.ceil(columns.length / 2)
-
-		switch (line[i]) {
-			case 'B':
-				rows = rows.slice(rowHalf, rows.length)
-				break
-			case 'F':
-				rows = rows.slice(0, rowHalf)
-				break
-			case 'R':
-				columns = columns.slice(colHalf, columns.length)
-				break
-			case 'L':
-				columns = columns.slice(0, colHalf)
-				break
+		for (let r = 1; r <= 128; r++) {
+			rows.push(r)
 		}
-	}
+		for (let c = 1; c <= 8; c++) {
+			columns.push(c)
+		}
 
-	let finalRow = rows[0] - 1
-	let finalCol = columns[0] - 1
-	let count = finalRow * 8 + finalCol
-	high = count > high ? count : high
-})
+		for (let i = 0; i < line.length; i++) {
+			let rowHalf = Math.ceil(rows.length / 2)
+			let colHalf = Math.ceil(columns.length / 2)
+
+			switch (line[i]) {
+				case 'B':
+					rows = rows.slice(rowHalf, rows.length)
+					break
+				case 'F':
+					rows = rows.slice(0, rowHalf)
+					break
+				case 'R':
+					columns = columns.slice(colHalf, columns.length)
+					break
+				case 'L':
+					columns = columns.slice(0, colHalf)
+					break
+			}
+		}
+
+		let finalRow = rows[0] - 1
+		let finalCol = columns[0] - 1
+		let count = finalRow * 8 + finalCol
+		high = count > high ? count : high
+	})
 console.log('high', high)
