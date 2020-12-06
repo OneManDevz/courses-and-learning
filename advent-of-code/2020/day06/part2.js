af90d33195ffaca1ece762c8f2b50ac0531cf823
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-12-06 07:11:35 +0100

    2020 day06 prettier

--- advent-of-code/2020/day06/part2.js
+++ advent-of-code/2020/day06/part2.js
@@ -1,28 +1,27 @@
-const groups = require('fs')
+let count = 0
+
+require('fs')
   .readFileSync('input.txt', 'utf-8')
   .split(/\n\s*\n/)
   .map((group) => group.split(/\s+/g))
   .map((group) => {
     return group.filter((person) => person.length > 0)
   })
+  .forEach((group) => {
+    const nOfApp = group.length
+    let groupAnswers = []
 
-let count = 0
+    let joinedAnswers = group.reduce((acc, curr) => (acc += curr))
 
-groups.forEach((group) => {
-  const nOfApp = group.length
-  let groupAnswers = []
-
-  let joinedAnswers = group.reduce((acc, curr) => (acc += curr))
-
-  for (let i = 0; i < joinedAnswers.length; i++) {
-    let regex = new RegExp(`^(.*${joinedAnswers[i]}.*){${nOfApp}}$`)
-    if (regex.test(joinedAnswers)) {
-      groupAnswers.push(joinedAnswers[i])
+    for (let i = 0; i < joinedAnswers.length; i++) {
+      let regex = new RegExp(`^(.*${joinedAnswers[i]}.*){${nOfApp}}$`)
+      if (regex.test(joinedAnswers)) {
+        groupAnswers.push(joinedAnswers[i])
+      }
     }
-  }
 
-  groupAnswers = [...new Set(groupAnswers)]
-  count += groupAnswers.length
-})
+    groupAnswers = [...new Set(groupAnswers)]
+    count += groupAnswers.length
+  })
 
 console.log('count', count)
