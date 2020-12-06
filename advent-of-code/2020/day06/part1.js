b571a89f48904f55e7240c19c549235a1f5f4e5d
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-12-06 07:05:27 +0100

    2020 day06 prettier

--- advent-of-code/2020/day06/part1.js
+++ advent-of-code/2020/day06/part1.js
@@ -1,26 +1,23 @@
-const groups = require('fs')
+let count = 0
+
+require('fs')
   .readFileSync('input.txt', 'utf-8')
   .split(/\n\s*\n/)
   .map((group) => group.split(/\s+/g))
   .map((group) => {
     return group.filter((person) => person.length > 0)
   })
+  .forEach((persons) => {
+    let groupAnswers = []
 
-let count = 0
+    persons.forEach((person) => {
+      for (let i = 0; i < person.length; i++) {
+        groupAnswers.push(person[i])
+      }
+    })
 
-groups.forEach((persons, i) => {
-  let groupAnswers = []
-  console.log('persons', persons)
-
-  persons.forEach((person) => {
-    for (let i = 0; i < person.length; i++) {
-      groupAnswers.push(person[i])
-    }
+    groupAnswers = [...new Set(groupAnswers)]
+    count += groupAnswers.length
   })
-
-  groupAnswers = [...new Set(groupAnswers)]
-  console.log('group:', i, ',answers count:', groupAnswers.length)
-  count += groupAnswers.length
-})
 
 console.log('count', count)
