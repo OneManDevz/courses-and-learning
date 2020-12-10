b1f9e4122a531d49efec49a7944a28c02b155db8
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-12-10 07:41:37 +0100

    part 1 clean up

--- advent-of-code/2020/day10/part1.js
+++ advent-of-code/2020/day10/part1.js
@@ -5,15 +5,13 @@ let adapters = require('fs')
 
 adapters.push(0) // outlet joltage
 adapters.push(Math.max(...adapters) + 3) // device's joltage
-console.log('adapters', adapters)
-adapters = adapters.sort((a, b) => (a > b ? 1 : -1))
+adapters = adapters.sort((a, b) => a - b)
 
 let joltage1 = 0
 let joltage2 = 0
 let joltage3 = 0
 
 for (let i = 1; i < adapters.length; i++) {
-  console.log(adapters[i + 1], adapters[i])
   if (adapters[i] - adapters[i - 1] == 1) {
     joltage1++
   } else if (adapters[i] - adapters[i - 1] == 2) {
